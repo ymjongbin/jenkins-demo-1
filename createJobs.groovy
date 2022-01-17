bb0477bfe7dd887626acf2c03fce4456161507aa
ymjongbin <ymjongbin@hotmail.com>
2022-01-17 10:55:44 -0500

    Update createJobs.groovy

--- createJobs.groovy
+++ createJobs.groovy
@@ -1,8 +1,14 @@
-pipelineJob('pipelineJob') {
+pipelineJob('theme-park-job') {
     definition {
-        cps {
-            script(readFileFromWorkspace('pipelineJob.groovy'))
-            sandbox()
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/tkgregory/spring-boot-api-example.git'
+                    }
+                    branch 'master'
+                }
+            }
         }
     }
 }
